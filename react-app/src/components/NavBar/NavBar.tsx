d9ec0855736b2f4971b6839d9a9a214a9b3b29a9
jdanusantoso <109101861+jdanusantoso@users.noreply.github.com>
2023-03-02 17:03:49 -0600

    Delete react-app directory

--- react-app/src/components/NavBar/NavBar.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom"
-
-export default function NavBar () {
-    return <div>
-        <Link to="/"> Home </Link>
-        <Link to="/pokeList"> Pokemon List </Link>
-    </div>
-}
