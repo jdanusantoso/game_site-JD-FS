ff2c50ceb482c9d639776b0da94df62f4ae7773a
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-03-02 17:01:39 -0600

    co-authored
    Co-authored-by: Fatimat Sheleru <fsheleru@gmail.com>

---
+++ rock-paper-scissors/src/Components/Player/Player.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Player.css';
+
+const Player = () => {
+  return (
+    <div className='playerid'>Game
+
+        <h1>Lets play a game</h1>
+    </div>
+
+  )
+}
+
+export default Player
